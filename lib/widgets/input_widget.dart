e9265e84925b747c192290dd6d9c7d77f6d81f3a
saharshkmr <kmrsaharsh@gmail.com>
2025-02-25 16:03:34 +0530

    Updated Input widget

--- lib/widgets/input_widget.dart
+++ lib/widgets/input_widget.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 
 Widget buildTextField(
-    String label,TextEditingController controller,
+    String label,
+    TextEditingController controller,
     String? Function(String?)? validator,
     void Function(String?) onSaved,
     {IconData? icon, int maxLines = 1, 
-    bool isNumeric = false, bool isPassword = false,})
-     {
+    bool isNumeric = false, bool isPassword = false, bool readOnly = false, void Function()? onTap}) {
   return SizedBox(
-    width: 300,
+    width: double.infinity,
     child: TextFormField(
       controller: controller,
       keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
       decoration: InputDecoration(
         labelText: label,
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-        prefixIcon: Icon(icon),
+        prefixIcon: icon != null ? Icon(icon) : null,
         contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
       ),
       maxLines: maxLines,
       obscureText: isPassword,
       validator: validator,
       onSaved: onSaved,
+      readOnly: readOnly,
+      onTap: onTap,
     ),
   );
 }
